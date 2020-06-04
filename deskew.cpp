22fc9665fa70612c8735b39b763e6c7ee6d869e7
James Manton <ajd.manton@googlemail.com>
2020-06-04 18:30:53 +0100

    Tidy up

--- deskew.cpp
+++ deskew.cpp
@@ -3,84 +3,79 @@
 using namespace cimg_library;
 
 namespace lsfm {
-  CImg<> deskew_fft(const CImg<> &raw_stack,
-		    const float pitch_xy,
-		    const float obj_angle,
-		    const float stage_step) {
-    const float slice_shift = stage_step * std::sin(obj_angle * M_PI / 180) / pitch_xy;
-    const int new_height = raw_stack.height() + ceil(fabs(slice_shift) * raw_stack.depth()) - 1;
+	/*
+		Use a 1D FFT and a ramped phase ramp to shift all slices together
+	*/
+	CImg<> deskew_fft(const CImg<> &raw_stack, const float pitch_xy, const float obj_angle, const float stage_step) {
+		const float slice_shift = stage_step * std::sin(obj_angle * M_PI / 180) / pitch_xy;
+		const int new_height = raw_stack.height() + ceil(fabs(slice_shift) * raw_stack.depth()) - 1;
 
-    printf("Deskewed height: %d\n", new_height);
-    printf("Slicewise shift: %4f\n", slice_shift);
+		printf("Deskewed height: %d\n", new_height);
+		printf("Slicewise shift: %4f\n", slice_shift);
 
-    int start_time = cimg::time();
-    CImgList<> deskewed = raw_stack.get_resize(-100, new_height, -100, -100, 0, 0).get_FFT('y');
-    cimg_forXYZ(deskewed[0], x, y, z) {
-      float k = y <= floor(new_height / 2) ? y : -new_height + y;
-      k *= -2 * M_PI / new_height;
-      float anti_z = raw_stack.depth() - 1 - z;
-      float a = deskewed[0](x, y, z);
-      float b = deskewed[1](x, y, z);
-      deskewed[0](x, y, z) = a * std::cos(k * slice_shift * anti_z) - b * std::sin(k * slice_shift * anti_z);
-      deskewed[1](x, y, z) = a * std::sin(k * slice_shift * anti_z) + b * std::cos(k * slice_shift * anti_z);
-    }
-    deskewed.FFT('y', true);
+		int start_time = cimg::time();
+		CImgList<> deskewed = raw_stack.get_resize(-100, new_height, -100, -100, 0, 0).get_FFT('y');
+		cimg_forXYZ(deskewed[0], x, y, z) {
+			float k = y <= floor(new_height / 2) ? y : -new_height + y;
+			k *= -2 * M_PI / new_height;
+			float anti_z = raw_stack.depth() - 1 - z;
+			float a = deskewed[0](x, y, z);
+			float b = deskewed[1](x, y, z);
+			deskewed[0](x, y, z) = a * std::cos(k * slice_shift * anti_z) - b * std::sin(k * slice_shift * anti_z);
+			deskewed[1](x, y, z) = a * std::sin(k * slice_shift * anti_z) + b * std::cos(k * slice_shift * anti_z);
+		}
+		deskewed.FFT('y', true);
 
-    int calc_time = cimg::time() - start_time;
-    printf("Deskewing time:  %d ms\n", calc_time);
-    return deskewed[0];
-  }
+		int calc_time = cimg::time() - start_time;
+		printf("Deskewing time:  %d ms\n", calc_time);
+		return deskewed[0];
+	}
 
-  //! Deskewing using linear interpolation
-  /**
-   **/
-  CImg<> deskew_linear(const CImg<> &raw_stack,
-		       const float pitch_xy,
-		       const float obj_angle,
-		       const float stage_step) {
-    const float slice_shift = stage_step * std::sin(obj_angle * M_PI / 180) / pitch_xy;
-    const int offset = ceil(fabs(slice_shift) * raw_stack.depth()) - 1;
-    const int new_height = raw_stack.height() + offset;
 
-    printf("Deskewed height: %d\n", new_height);
-    printf("Slicewise shift: %4f\n", slice_shift);
+	/*
+		Use a 1D linear interpolation to quickly calculate shifted values
+	*/
+	CImg<> deskew_linear(const CImg<> &raw_stack, const float pitch_xy, const float obj_angle, const float stage_step) {
+		const float slice_shift = stage_step * std::sin(obj_angle * M_PI / 180) / pitch_xy;
+		const int offset = ceil(fabs(slice_shift) * raw_stack.depth()) - 1;
+		const int new_height = raw_stack.height() + offset;
 
-    CImg<> deskewed(raw_stack.width(), new_height, raw_stack.depth(), 1, 0);
-    int start_time = cimg::time();
+		printf("Deskewed height: %d\n", new_height);
+		printf("Slicewise shift: %4f\n", slice_shift);
 
-    // No interpolation
-    // cimg_forXYZ(raw_stack, x, y, z) {
-    // 		int deskewed_y = offset + round(y - z * slice_shift);
-    // 		deskewed(x, deskewed_y) = raw_stack(x, y, z) > deskewed(x, deskewed_y) ? raw_stack(x, y, z) : deskewed(x, deskewed_y);
-    // }
+		CImg<> deskewed(raw_stack.width(), new_height, raw_stack.depth(), 1, 0);
+		int start_time = cimg::time();
 
-    // Linear interpolation
-    float y0 = slice_shift;
-    float y1 = floor(slice_shift);
-    float y2 = ceil(slice_shift);
-    cimg_forXYZ(raw_stack, x, y, z) {
-      float f1 = raw_stack(x, y, z);
-      float f2 = y < raw_stack.height() - 1 ? raw_stack(x, y + 1, z) : 0;
-      float interp = f1 * (y2 - y0) / (y2 - y1) + f2 * (y0 - y1) / (y2 - y1);
+		// No interpolation
+		// cimg_forXYZ(raw_stack, x, y, z) {
+		// 		int deskewed_y = offset + round(y - z * slice_shift);
+		// 		deskewed(x, deskewed_y) = raw_stack(x, y, z) > deskewed(x, deskewed_y) ? raw_stack(x, y, z) : deskewed(x, deskewed_y);
+		// }
 
-      int deskewed_y = offset + round(y - z * slice_shift);
-      deskewed(x, deskewed_y, z) = interp;
-    }
+		// Linear interpolation
+		float y0 = slice_shift;
+		float y1 = floor(slice_shift);
+		float y2 = ceil(slice_shift);
+		cimg_forXYZ(raw_stack, x, y, z) {
+			float f1 = raw_stack(x, y, z);
+			float f2 = y < raw_stack.height() - 1 ? raw_stack(x, y + 1, z) : 0;
+			float interp = f1 * (y2 - y0) / (y2 - y1) + f2 * (y0 - y1) / (y2 - y1);
 
-    int calc_time = cimg::time() - start_time;
-    printf("Deskew time:     %d ms\n", calc_time);
-    return deskewed;
-  }
+			int deskewed_y = offset + round(y - z * slice_shift);
+			deskewed(x, deskewed_y, z) = interp;
+		}
 
-  CImg<> deskew(const CImg<> &raw_stack,
-		const float pitch_xy,
-		const float obj_angle,
-		const float stage_step,
-		const char * method) {
-    if (!strcmp(method, "fft")) {
-      return deskew_fft(raw_stack, pitch_xy, obj_angle, stage_step);
-    } else {
-      return deskew_linear(raw_stack, pitch_xy, obj_angle, stage_step);
-    }
-  }
+		int calc_time = cimg::time() - start_time;
+		printf("Deskew time:     %d ms\n", calc_time);
+		return deskewed;
+	}
+
+
+	CImg<> deskew(const CImg<> &raw_stack, const float pitch_xy, const float obj_angle, const float stage_step, const char * method) {
+		if (!strcmp(method, "fft")) {
+			return deskew_fft(raw_stack, pitch_xy, obj_angle, stage_step);
+		} else {
+			return deskew_linear(raw_stack, pitch_xy, obj_angle, stage_step);
+		}
+	}
 }
--- lsfm.cpp
+++ lsfm.cpp
@@ -1,78 +1,79 @@
 #include "lsfm.h"
 
+
 int deskew(int argc, char * argv[]) {
-  cimg_help("Deskew stage scan light sheet data");
-  const char * method = cimg_option("-method", "", "method []");
-  const char * file_img = cimg_option("-i", (char*) 0, "input image file");
-  const char * file_out = cimg_option("-o", (char*) 0, "output image file");
-  const float pitch_xy = cimg_option("-xy", (float)104, "xy pixel pitch");
-  const float obj_angle = cimg_option("-a", (float)58.2, "objective angle");
-  const float stage_step = cimg_option("-s", (float)500, "stage step");
-  const bool display = cimg_option("-d", false, "display MIP");
-  if (!file_img || !file_out) {return 1;}
+	cimg_help("Deskew stage scan light sheet data");
+	const char * method = cimg_option("-method", "", "method []");
+	const char * file_img = cimg_option("-i", (char*) 0, "input image file");
+	const char * file_out = cimg_option("-o", (char*) 0, "output image file");
+	const float pitch_xy = cimg_option("-xy", (float)104, "xy pixel pitch");
+	const float obj_angle = cimg_option("-a", (float)58.2, "objective angle");
+	const float stage_step = cimg_option("-s", (float)500, "stage step");
+	const bool display = cimg_option("-d", false, "display MIP");
+	if (!file_img || !file_out) {return 1;}
 
-  CImg<> img = lsfm::load_tiff(file_img);
-  img = lsfm::deskew(img, pitch_xy, obj_angle, stage_step, method);
-  lsfm::save_tiff(img, file_out);
-  
+	CImg<> img = lsfm::load_tiff(file_img);
+	img = lsfm::deskew(img, pitch_xy, obj_angle, stage_step, method);
+	lsfm::save_tiff(img, file_out);
 
-  if (display) {
-    img.display();
-    printf("\n");
-  }
 
-  return 0;
+	if (display) {
+		img.display();
+		printf("\n");
+	}
+
+	return 0;
 }
+
 
 int deconv(int argc, char * argv[]) {
-  return 0;
+	return 0;
 }
+
 
 int proj(int argc, char * argv[]) {
-  cimg_help("Project stack");
-  const char * method = cimg_option("-method", "", "method []");
-  const char * file_img = cimg_option("-i", (char*) 0, "input image file");
-  const char * file_out = cimg_option("-o", (char*) 0, "output image file");
-  const float pitch_xy = cimg_option("-xy", (float)104, "xy pixel pitch");
-  const bool display = cimg_option("-d", false, "display projection");
-  if (!file_img || !file_out) {return 1;}
+	cimg_help("Project stack");
+	const char * method = cimg_option("-method", "", "method []");
+	const char * file_img = cimg_option("-i", (char*) 0, "input image file");
+	const char * file_out = cimg_option("-o", (char*) 0, "output image file");
+	const float pitch_xy = cimg_option("-xy", (float)104, "xy pixel pitch");
+	const bool display = cimg_option("-d", false, "display projection");
+	if (!file_img || !file_out) {return 1;}
 
-  CImg<> img = lsfm::load_tiff(file_img);
-  img = lsfm::proj(img, method);
-  lsfm::save_tiff(img, file_out);
+	CImg<> img = lsfm::load_tiff(file_img);
+	img = lsfm::proj(img, method);
+	lsfm::save_tiff(img, file_out);
 
-  if (display) {
-    img.display();
-    printf("\n");
-  }
+	if (display) {
+		img.display();
+		printf("\n");
+	}
 
-  return 0;
+	return 0;
 }
 
+
 int main(int argc, char * argv[]) {
+	if (argc == 1) {
+		printf("\nlsfm [command] [options] \n\n"
+			   "Use one of the following commands:\n"
+			   "  deskew             : deskew stack\n"
+			   "  deconv             : deconvolve stack\n"
+			   "  proj               : project stack\n"
+			   "  deskew+deconv      : deskew and deconvolve stack\n"
+			   "  deskew+proj        : deskew and project stack\n"
+			   "  deskew+deconv+proj : deskew, deconvolve and project stack\n\n"
+			   "Multi-operation commands will save each step as a separate file.\n\n");
+		return 0;
+	}
 
-  if (argc == 1) {
-    printf("\nlsfm [command] [options] \n\n"
-	   "Use one of the following commands:\n"
-	   "  deskew             : deskew stack\n"
-	   "  deconv             : deconvolve stack\n"
-	   "  proj               : project stack\n"
-     "  deskew+deconv      : deskew and deconvolve stack\n"
-     "  deskew+proj        : deskew and project stack\n"
-     "  deskew+deconv+proj : deskew, deconvolve and project stack\n\n"
-	   "Multi-operation commands will save each step as a separate file.\n\n");
-    return 0;
-  }
+	if (!strcmp(argv[1], "deskew")) {
+		return deskew(argc, argv);
+	} else if (!strcmp(argv[1], "deconv")) {
+		return deconv(argc, argv);
+	} else if (!strcmp(argv[1], "proj")) {
+		return proj(argc, argv);
+	}
 
-  if (!strcmp(argv[1], "deskew")) {
-    return deskew(argc, argv);
-  } else if (!strcmp(argv[1], "deconv")) {
-    return deconv(argc, argv);
-  } else if (!strcmp(argv[1], "proj")) {
-    return proj(argc, argv);
-  } else {
-    return process(argc, argv);
-  }
-
-  return 0;
+	return 0;
 }
