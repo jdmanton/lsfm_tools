5faa3d00bfc29eb9c947978588010a3a89a1d332
James Manton <ajd.manton@googlemail.com>
2020-06-17 14:54:09 +0100

    Add show command and tidy help

--- lsfm.cpp
+++ lsfm.cpp
@@ -1,15 +1,14 @@
 #include "lsfm.h"
 
-
 int deskew(int argc, char* argv[]) {
-	cimg_help("Deskew stage scan light sheet data");
-	const char* method =     cimg_option("-m", "", "method [linear, nn, fourier]");
+	cimg_help("\nDeskew stage scan light sheet data");
+	const char* method =     cimg_option("-m", "linear", "method [linear, nn, fourier]");
 	const char* file_img =   cimg_option("-i", (char*) 0, "input image file");
 	const char* file_out =   cimg_option("-o", (char*) 0, "output image file");
 	const float pitch_xy =   cimg_option("-xy", (float) 104, "xy pixel pitch");
 	const float obj_angle =  cimg_option("-a", (float) 58.2, "objective angle");
 	const float stage_step = cimg_option("-s", (float) 500, "stage step");
-	const bool display =     cimg_option("-d", false, "display deskewed stack");
+	const bool display =     cimg_option("-d", false, "display deskewed stack\n");
 	if (!file_img || !file_out) {return 1;}
 
 	CImg<> img = lsfm::load_tiff(file_img);
@@ -30,12 +29,12 @@ int deconv(int argc, char* argv[]) {
 
 
 int proj(int argc, char* argv[]) {
-	cimg_help("Project stack");
-	const char* method =   cimg_option("-m", "", "method []");
+	cimg_help("\nProject stack to 2D image");
+	const char* method =   cimg_option("-m", "mip", "method [mip, mean, laplacian]");
 	const char* file_img = cimg_option("-i", (char*) 0, "input image file");
 	const char* file_out = cimg_option("-o", (char*) 0, "output image file");
 	const float pitch_xy = cimg_option("-xy", (float) 104, "xy pixel pitch");
-	const bool display =   cimg_option("-d", false, "display projection");
+	const bool display =   cimg_option("-d", false, "display projection\n");
 	if (!file_img || !file_out) {return 1;}
 
 	CImg<> img = lsfm::load_tiff(file_img);
@@ -49,21 +48,37 @@ int proj(int argc, char* argv[]) {
 }
 
 
+int show(int argc, char* argv[]) {
+	cimg_help("\nDisplay stack");
+	CImg<> img = lsfm::load_tiff(argv[2]);
+	img.display("Stack", false);
+	printf("\n");
+	return 0;
+}
+
+
 int main(int argc, char* argv[]) {
 	if (argc == 1) {
 		printf("\nlsfm [command] [options] \n\n"
 			   "Use one of the following commands:\n"
+			   "  show               : show stack\n"
 			   "  deskew             : deskew stack\n"
 			   "  deconv             : deconvolve stack\n"
 			   "  proj               : project stack\n"
 			   "  deskew+deconv      : deskew and deconvolve stack\n"
 			   "  deskew+proj        : deskew and project stack\n"
+			   "  deconv+proj        : deconvolve and project stack\n"
 			   "  deskew+deconv+proj : deskew, deconvolve and project stack\n\n"
-			   "Multi-operation commands will save each step as a separate file.\n\n");
+			   "Multi-operation commands will save each step as a separate file.\n"
+			   "Use -h as an option to learn about each command.\n\n");
 		return 0;
 	}
 
-	if (!strcmp(argv[1], "deskew")) {
+	if (!strcmp(argv[1], "-h")) {
+		main(1, argv);
+	} else if (!strcmp(argv[1], "show")) {
+		return show(argc, argv);
+	} else if (!strcmp(argv[1], "deskew")) {
 		return deskew(argc, argv);
 	} else if (!strcmp(argv[1], "deconv")) {
 		return deconv(argc, argv);
